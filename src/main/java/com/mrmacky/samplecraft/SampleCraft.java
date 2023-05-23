265ca8734280426b0b9d5ccc413ed7ada149f62b
Syric <134160470+CMCanterbury@users.noreply.github.com>
2023-05-23 13:28:59 -0400

    Tried to Add Shrimp

--- src/main/java/com/mrmacky/samplecraft/SampleCraft.java
+++ src/main/java/com/mrmacky/samplecraft/SampleCraft.java
@@ -19,6 +19,7 @@ public class SampleCraft
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        SCItems.register(modEventBus);
     }
 
 }
